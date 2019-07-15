30b2cd23e90d87b3591b27740a8f91ce2c67370f
tkwbdv <mick.18@gmx.de>
2019-07-15 13:14:08 +0200

    cart-items are now displayed inside the cart-dropdown

--- src/components/CartDropdown.js
+++ src/components/CartDropdown.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import CustomButton from "./CustomButton";
+import CartItem from "./CartItem";
 
-const CartDropdown = ({ isCartHidden }) => (
+const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown" >
-    <div className="cart-items" />
+    <div className="cart-items">
+      {cartItems
+        .map(item => <CartItem key={item.id} item={item} />)}
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div >
 );
 
-export default CartDropdown;
+const mapStateToProps = state => ({
+  cartItems: state.cart.cartItems
+});
+
+export default connect(mapStateToProps)(CartDropdown);
