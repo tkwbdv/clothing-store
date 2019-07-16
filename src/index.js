a5a179278097d50e97d5454b4ebdf913f698dec4
tkwbdv <mick.18@gmx.de>
2019-07-16 15:59:26 +0200

    integrated persistent state with redux-persist

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import store from "./redux/configureStore";
+import { store, persistor } from "./redux/configureStore";
 import AppRouter from "./routers/AppRouter"
 import "normalize.css";
 import "./styles/styles.scss";
@@ -11,7 +12,9 @@ import "./styles/styles.scss";
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <AppRouter />
+      <PersistGate persistor={persistor}>
+        <AppRouter />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,15 +1,28 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import userReducer from "../reducers/userReducer";
 import cartReducer from "../reducers/cartReducer";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"]
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
-}),
+});
+
+const store = createStore(
+  persistReducer(persistConfig, rootReducer),
   composeEnhancers(applyMiddleware())
 );
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
