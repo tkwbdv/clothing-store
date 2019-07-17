7b4798909b1350eb4560f77224b509759673df08
tkwbdv <mick.18@gmx.de>
2019-07-17 19:20:25 +0200

    added stripe for payments to checkout-page. updated favicon.

---
+++ src/components/StripeCheckoutButton.js
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_tU4uwNM0tcdusnLv99fSCyV200MLy38aGc";
+
+  const onToken = token => {
+    console.log(token);
+    alert("Payment Successful");
+  }
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Clothing Store"
+      billingAddress
+      shippingAddress
+      image="/images/logo.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+}
+
+export default StripeCheckoutButton;
--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -6,6 +6,7 @@ import { selectCartItems } from "../selectors/cartSelectors";
 import { selectCartTotal } from "../selectors/cartSelectors";
 
 import CheckoutItem from "../components/CheckoutItem";
+import StripeCheckoutButton from "../components/StripeCheckoutButton";
 
 const CheckoutPage = ({ cartItems, cartTotal }) => (
   <div className="checkout-page">
@@ -35,6 +36,12 @@ const CheckoutPage = ({ cartItems, cartTotal }) => (
     <div className="total">
       <span>TOTAL: ${cartTotal}</span>
     </div>
+    <div className="test-warning">
+      *Please use the following test credit card for payments*
+      <br />
+      Number: 4242 4242 4242 4242 - Exp: 10 / 20 - CVC: 123
+    </div>
+    <StripeCheckoutButton price={cartTotal} />
   </div>
 );
 
