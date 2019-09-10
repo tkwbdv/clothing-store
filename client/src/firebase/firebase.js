b0992ec9ccc84ec858ae1a29aa8fe2df47a17214
tkwbdv <mick.18@gmx.de>
2019-09-10 17:02:24 +0200

    load firebase config from env variable

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -4,7 +4,7 @@ import "firebase/auth";
 
 // FIREBASE CONFIG
 
-/* const firebaseConfig = {
+const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
@@ -12,17 +12,17 @@ import "firebase/auth";
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID
-}; */
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBUrIRMFX9Fg_KlqzYu6VLS71anifrtAHg",
-  authDomain: "clothing-store-7252a.firebaseapp.com",
-  databaseURL: "https://clothing-store-7252a.firebaseio.com",
-  projectId: "clothing-store-7252a",
-  storageBucket: "clothing-store-7252a.appspot.com",
-  messagingSenderId: "409260899055",
-  appId: "1:409260899055:web:956db8723f7642d1"
 };
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBUrIRMFX9Fg_KlqzYu6VLS71anifrtAHg",
+//   authDomain: "clothing-store-7252a.firebaseapp.com",
+//   databaseURL: "https://clothing-store-7252a.firebaseio.com",
+//   projectId: "clothing-store-7252a",
+//   storageBucket: "clothing-store-7252a.appspot.com",
+//   messagingSenderId: "409260899055",
+//   appId: "1:409260899055:web:956db8723f7642d1"
+// };
 
 firebase.initializeApp(firebaseConfig);
 
